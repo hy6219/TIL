413f7923ea2b62a7c32ce43306eaee2d3dc689a0
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-03-02 14:20:59 +0900

    Create ExceptionEx1.java
    try-catch 문 기본 골격

---
+++ JAVA/Exception/ExceptionEx1.java
@@ -0,0 +1,37 @@
+package com.exception01;
+/**
+try-catch 문 기본 골격
+*/
+public class ExceptionEx1 {
+
+	/**
+	 * 
+	 * @param args
+	 * try-catch문의 기본적인 형태들
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			try {
+				
+			}catch(Exception e)
+			{
+				
+			}
+		}catch(Exception e1)
+		{
+			
+		}
+		
+		try
+		{
+			
+		}
+		catch(Exception e2)
+		{
+			
+		}
+	}
+
+}
