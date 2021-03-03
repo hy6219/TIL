0f1e6e47e21f1e0afdf3f6fcf3c6c37b12758a1d
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-03-03 19:47:55 +0900

    고의로 런타임 에러 발생
    -RuntimeException & 자손들--> 프로그래머의 실수로 발생한 'unchecked' 예외--> 예외처리 강제x-> 따라서, 성공적으로 컴파일됨(unresolved compilation 문구가 뜨지 않음)
    -Exception & 자손들-->외부 영향으로 발생한 'checked' 예외

---
+++ JAVA/Exception/ExceptionEx13.java
@@ -0,0 +1,10 @@
+package com.exception08;
+
+public class ExceptionEx13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		throw new RuntimeException();
+	}
+
+}
