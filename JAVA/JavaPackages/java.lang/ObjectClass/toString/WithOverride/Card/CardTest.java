16c36690f4c4081332165af90c91ae175f8f92a2
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-03-07 15:40:53 +0900

    toString with override

---
+++ JAVA/JavaPackages/java.lang/ObjectClass/toString/WithOverride/Card/CardTest.java
@@ -0,0 +1,11 @@
+package com.objectClass.toString.withOverride;
+
+public class CardTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Card c = new Card();
+		System.out.println("c.toString: "+c.toString());
+	}
+
+}
