7db0e719fe0533e77e191ef9349b5c1054e62887
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-03-11 14:07:34 +0900

    String overview

---
+++ JAVA/JavaPackages/java.lang/ObjectClass/String/readme.java
@@ -0,0 +1,21 @@
+package com.objectClass.string;
+
+public class readme {
+/**
+ * String: 
+ * Immutable 객체!
+ * public final class String implements java.io.Serializable, Comparable
+ * {
+ * }
+ * -->다른 클래스의 조상이 될 수 없음
+ * 
+ * 한번 생성된 String 인스턴스가 갖고 있는 문자열은 읽어올 수만 있고
+ * 변경할 수 없음
+ * 
+ * String s = literal -->클래스가 메모리에 로드될 때 자동으로 미리 생성됨
+ * 
+ * 
+ * 
+ * StringBuffer 인스턴스: 문자열을 다루는 작업이 많이 필요한 경우 유용함
+ */
+}
