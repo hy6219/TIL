b41d5c4d07a4c5f7ff1a246fe8dd9fb0d6f75fd1
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-03-12 21:14:06 +0900

    Update parsingBetween.java

--- JAVA/JavaPackages/java.lang/ObjectClass/String/parsingBetween.java
+++ JAVA/JavaPackages/java.lang/ObjectClass/String/parsingBetween.java
@@ -11,7 +11,12 @@ public class parsingBetween {
 		String t= String.valueOf(i);
 		String l = d + "";
 		//String to i
-		int j = Integer.parseInt(s);
+		/**
+		 * 
+		 * Wrapper class. parseInt/... 사용시 ->trim으로
+		 * 공백을 제거해주어야 함 (NumberFormatException 발생 가능)
+		 */
+		int j = Integer.parseInt(s.trim());
 		int k = Integer.valueOf(s);
 		
 		System.out.println(s);
@@ -19,6 +24,15 @@ public class parsingBetween {
 		System.out.println(l);
 		System.out.println(j);
 		System.out.println(k);
+		
+		String f = "false";
+		boolean b= Boolean.valueOf(f);
+		System.out.println(b);
+		
+		/**
+		 * Boolean, Integer, ... ->Wrapper class : 기본형 값을 감싸고 있는 클래스
+		 */
+		System.out.println(Boolean.parseBoolean(f));
 	}
 
 }
