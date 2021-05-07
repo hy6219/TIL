7bce92b77c20040759f085848b95f17d17492e6f
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-05-07 11:47:15 +0900

    게시글 작성 정보를 담을 자바 빈 객체에 대한 클래스

---
+++ JSP Servlet/JavaBean/makeEditor/ArticleBean.java
@@ -0,0 +1,41 @@
+package com.jisoo.javabeans;
+
+public class ArticleBean {
+	private String author;
+	private String pwd;
+	private String email;
+	private String title;
+	private String contents;
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	
+	
+}
