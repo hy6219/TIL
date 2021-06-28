880438b3103bc12b75618ffe849bcb81ac8fbfc1
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-06-28 18:16:45 +0900

    기존 자바 컬렉션에 없던 특정 인덱스 요소 제거

--- Algorithm/General/Data_Structure/Queue_Deque/com/dataStructure/queue/generics/GenericQueueTest.java
+++ Algorithm/General/Data_Structure/Queue_Deque/com/dataStructure/queue/generics/GenericQueueTest.java
@@ -15,6 +15,15 @@ public class GenericQueueTest {
         System.out.println("isEmpty?:"+ customQueue.isEmpty());//isEmpty?:false
         System.out.println("size: "+customQueue.size());//size: 2
 
+        System.out.println("test remove");
+        customQueue.offer("test remove");
+        customQueue.offer("test");
+
+        System.out.println(customQueue.remove(2));//특별히 추가한 특정 인덱스의 값 제거-test remove가 지워질것
+        //test remove
+        System.out.println(customQueue);//[Hello Queue!,Make Custom Queue using Generics!,test]
+
+
         int sizeCurr= customQueue.size();
 
         for(int i = 0 ; i < sizeCurr; i++){
