8c325c68dc09905e1366d93591e668eb87b9db07
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-07-08 12:01:43 +0900

    SWEA1933번-간단한 N 의 약수

---
+++ Algorithm/SWEA/D1/SWEA1933/Solution.java
@@ -0,0 +1,20 @@
+package com.swea.swea1933;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        StringBuilder sb= new StringBuilder();
+
+        sb.append(1+" ");
+        for(int i = 2 ; i <n; i++){
+            if(n%i==0) {
+                sb.append(i+" ");
+            }
+        }
+        sb.append(n);
+        System.out.println(sb);
+    }
+}
