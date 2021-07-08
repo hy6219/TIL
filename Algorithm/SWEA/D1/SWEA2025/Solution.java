cdcd6bd6c739a7bd884450d716ba80b188f4f0c7
JSJeong <39812154+hy6219@users.noreply.github.com>
2021-07-08 11:42:45 +0900

    SWEA2025번-N줄덧셈

---
+++ Algorithm/SWEA/D1/SWEA2025/Solution.java
@@ -0,0 +1,14 @@
+package com.swea.swea2025;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args){
+        //공차수열을 이용해서 n*(n+1)/2
+        Scanner s = new Scanner(System.in);
+        int    n = s.nextInt();
+
+        System.out.println(n*(n+1)/2);
+
+    }
+}
